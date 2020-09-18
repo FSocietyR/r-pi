0b7218e9fd24961cf44728fa1d64d9899a26985e
FSocietyR <48395407+FSocietyR@users.noreply.github.com>
2020-09-18 09:40:49 +0300

    Update vp.py

--- vp.py
+++ vp.py
@@ -1,66 +1,119 @@
 import cv2 as cv
 import numpy as np
-from matplotlib import pyplot as plt
 import time
 import imutils
+
 	#creating window	
 window = cv.namedWindow("countrs")
-
 	# creating videocapture from main camera
 
-cap = cv.VideoCapture(0);
+cap = cv.VideoCapture(0)
+
 counter = 0
-def photo_analysis(im1, im2):
-	im1 = np.around(np.divide(im1, 50.0), decimals = 1)
-	im2 = np.around(np.divide(im2, 50.0), decimals = 1)
+
+def reworking(img):
 	
-	return im2 - im1
+	gray = cv.cvtColor(img1,cv.COLOR_BGR2GRAY)
+	blurred = cv.GaussianBlur(gray, (11,11),50)
+	median = cv.medianBlur(blurred,5)
+	last = np.around(np.divide(median, 50.0), decimals = 1)
+	
+	return last
+
 """
+
 	Основные идеи:
+
 		подклюить гугловский апи
+
 		искать фото в гугле
+
 		взять первые 10 фото
+
 		сравнить схожесть с пооью ИИ или же BFmatcher, но только если бюудет работать,
+
 		да и надо будет как то подклюить интернет к коробоке
+
 	"""
+
+remake_to_np_ms = lambda x: np.around(np.divide(x,50.0), decimals = 1)
 
 while True:
 
-	ret,img1 = cap.read()
-				
-	
+	ret,img1 = cap.read() #takes photo from video
+	image = reworking(img1)
+	if counter == 0:
+		img_matrix = []
+		img_matrix.append(image)
+	elif counter % 1 == 0:
+		if len(img_matrix) == 1:
+			img_matrix.append(image)
+		else:
+			img_matrix[0] = img_matrix[-1]
+			img_matrix[-1] = (image)
+		
+		diff = img_matrix[-1]-img_matrix[0]
+		diff[diff<0] = 0
+		
+		cv.imshow('a',img_matrix[0])
+		cv.imshow('b',img_matrix[-1])
+		cv.imshow('c',diff)	 	
 	#starting photo analys
-	
 	#res  = photo_analysis(img1)
-	res = 1
-	if res  == 1:
-		
-		gray = cv.cvtColor(img1,cv.COLOR_BGR2GRAY)
 
-		# working with an image
-		blurred = cv.GaussianBlur(gray, (11,11),50)
-		median = cv.medianBlur(blurred,5)
+		res = 1
+
+		if res  == 1:
+
+			
+			# working with an image
+			# creating numpy masssive which takes information from blurreds
+
+			pixels1 = np.around(np.divide(diff, 0.5), decimals = 1)
+			# modifying the image
+			def draw():
+				low_white = np.array(2,np.uint8) #for rasberry use 4
+
+				max_white = np.array(10,np.uint8)
+
+				mask = cv.inRange(pixels1,(low_white), (max_white))	
+
+				cv.imshow('main1', mask)
+
+				cnts,hierarchy = cv.findContours(mask.copy(), cv.RETR_EXTERNAL,
+
+					cv.CHAIN_APPROX_SIMPLE)	
+
+				#cv.drawContours(img1,cnts,-1,(255,0,0),3,cv.LINE_AA,hierarchy,1)
+				try:
+					for el in range(len(cnts)):
+						if cv.contourArea(cnts[el]) > 300:
+							cv.drawContours(img1,cnts,el,(255,0,0),3,cv.LINE_AA,hierarchy,1)
+				except IndexError:
+					pass
+			draw()
+			cv.imshow('cntr',img1)
+			cv.imshow('countrs', image)
+			
+
 		
-		# creating numpy masssive which takes information from blurreds
-		pixels1 = np.around(np.divide(median, 50.0), decimals = 1)
-		# modifying the image
-		low_white = np.array(4,np.uint8)
-		max_white = np.array(10,np.uint8)
-		mask = cv.inRange(pixels1,(low_white), (max_white))	
-		cv.imshow('main1', mask)
-		cnts,hierarchy = cv.findContours(mask.copy(), cv.RETR_EXTERNAL,
-			cv.CHAIN_APPROX_SIMPLE)	
-		
-		cv.drawContours(img1,cnts,-1,(255,0,0),3,cv.LINE_AA,hierarchy,1)
-		cv.imshow('countrs', img1)
-		
-		counter += 1
-					
-		if  not ret:
-			breaK
-		k = cv.waitKey(1)
-		if k%256 == 27:
-		 break
+
+	counter += 1
+
+				
+
+	if  not ret:
+
+		breaK
+
+	k = cv.waitKey(1)
+
+	if k%256 == 27:
+
+	 break
+
 """
+
 позднее добавить функции аналиха фотографий, в пларне, нужные - оставить, (полезные)
+
 нужные же отпралять на аналих"""
