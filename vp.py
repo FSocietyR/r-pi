538012ceebfc196163aa8df858663b2a04eab7ca
FSocietyR <48395407+FSocietyR@users.noreply.github.com>
2020-09-19 16:03:51 +0300

    Update vp.py

--- vp.py
+++ vp.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import random
 import numpy as np
 import time
 import imutils
@@ -8,140 +9,140 @@ import picamera as pi
 from picamera import PiCamera
 from picamera.array import PiRGBArray
 
-	#creating window	
+# from any_file import func()
 
-window = cv.namedWindow("countrs")
-
-	# creating videocapture from main camera
-counter = 0
-
+#PiCamera class
 class Camera(PiCamera):
 
-	def __init__(self,
-				  shutter_speed:int,
-				   framerate:int,
-				     exposure_mode:str,
-					  iso: int,
-					   saturation: int,
-					    sharpness: int,
-						 contrast: int,
-						  brightness: int):
-		
-		PiCamera.__init__(self)
+        def __init__(self,
+                    shutter_speed:int,
+                    framerate:int,
+                        iso: int,
+                        saturation: int,
+                            sharpness: int,
+                            contrast: int,
+                            brightness: int) -> object:
+            
+            PiCamera.__init__(self)
 
-		self.resolution = (640,480)
-		self.shutter_speed = shutter_speed #скорость затвора 
-		self.framerate = framerate #fps
-		self.exposure_mode = exposure_mode #экспозиция 
-		self.iso = iso #баланс белого
-		self.saturation = saturation #насыщенность
-		self.sharpness = sharpness #резкость
-		self.contrast = contrast #контраст
-		self.brightness = brightness #яркость 
-		
-	def rawRGB(self):
-		return PiRGBArray(self, size = self.resolution)				
+            #mostly turns off all camera properties
+            self.flash_modes = 'off'
+            self.exposure_mods = 'night'
+            self.awb_modes = 'off'
+            self.image_effects = 'negativ'
+            self.drc_settings = 'off'
+            self.stereo_mods = 'off' 
 
-camera = Camera(10000000, 30, 'night', 100,-100,-100,100,10)
+            self.resolution = (640,480)
+            self.shutter_speed = shutter_speed #скорость затвора 
+            self.framerate = framerate #fps 
+            self.iso = iso #баланс белого
+            self.saturation = saturation #насыщенность
+            self.sharpness = sharpness #резкость
+            self.contrast = contrast #контраст
+            self.brightness = brightness #яркость
 
-def reworking(img):
-
-	
-
-	gray = cv.cvtColor(img1,cv.COLOR_BGR2GRAY)
-	blurred = cv.GaussianBlur(gray, (11,11),50)
-	median = cv.medianBlur(blurred,5)
-	last = np.around(np.divide(median, 50.0), decimals = 1)
-
-	
-
-	return last
+            
+        def rawRGB(self) -> NoneType:
+                return PiRGBArray(self, size = self.resolution)				
 
 
 
-def taking_photo(img):
-
-	pass
-
-"""
-
-	Основные идеи:
-		подклюить гугловский апи
-		искать фото в гугле
-		взять первые 10 фото
-		сравнить схожесть с пооью ИИ или же BFmatcher, но только если бюудет работать,
-		да и надо будет как то подклюить интернет к коробоке
-
-	"""
+if __name__ == '__name__':
 
 
+    window = cv.namedWindow("countrs") #main_frame
+    counter = 0
+    camera = Camera(10000000, 30, 100,-100,-100,100,10) #creating a PiCamera object
 
-remake_to_np_ms = lambda x: np.around(np.divide(x,50.0), decimals = 1)
+    def reworking(img:list) -> np.array:
+    	"""
+		So, too use other fucntions and find anything 
+		in photos we need to change our main picture
+		named as img1, whic is taken from picamera		
 
-raw = camera.rawRGB()
-for frame in camera.capture_continuous(raw,format='bgr', use_video_port = True):
-	img1 = frame.array
-	cv.imshow('rand',img1)
-	image = reworking(img1)
+    	"""
+        gray = cv.cvtColor(img1,cv.COLOR_BGR2GRAY)
+        blurred = cv.GaussianBlur(gray, (11,11),50)
+        median = cv.medianBlur(blurred,5)
+        last = np.around(np.divide(median, 50.0), decimals = 1)
+        return last
 
-	if counter == 0:
-		img_matrix = []
-		img_matrix.append(image)
+    remake_to_np_ms = lambda x: np.around(np.divide(x,50.0), decimals = 1)
 
-	elif counter % 1 == 0:
-		if len(img_matrix) == 1:
-			img_matrix.append(image)
+    raw = camera.rawRGB()
+    for frame in camera.capture_continuous(raw,format='bgr', use_video_port = True):
+        img1 = frame.array 
+        cv.imshow('rand',img1)
+        image = reworking(img1)
 
-		else:
-			img_matrix[0] = img_matrix[-1]
-			img_matrix[-1] = (image)
+        if counter == 0:
+            img_matrix = []
+            img_matrix.append(image)
 
+        elif counter % 1 == 0:
+            if len(img_matrix) == 1:
+                img_matrix.append(image)
 
-
-		diff = img_matrix[-1]-img_matrix[0]
-		diff[diff<0] = 0
+            else:
+                img_matrix[0] = img_matrix[-1]
+                img_matrix[-1] = (image)
 
 
 
-		cv.imshow('a',img_matrix[0])
-		cv.imshow('b',img_matrix[-1])
-		cv.imshow('c',diff)	 	
+            diff = img_matrix[-1]-img_matrix[0]
+            diff[diff<0] = 0
 
-	#starting photo analys
-	#res  = photo_analysis(img1)
-		res = 1
-		if res  == 1:
 
-			# working with an image
-			# creating numpy masssive which takes information from blurreds
-			pixels1 = np.around(np.divide(diff, 0.1), decimals = 1)
-			cv.imshow("pixels",pixels1)
-			# modifying the image
 
-			def draw():
+            cv.imshow('a',img_matrix[0])
+            cv.imshow('b',img_matrix[-1])
+            cv.imshow('c',diff)	 	
 
-				low_white = np.array(4,np.uint8) #for rasberry use 4
-				max_white = np.array(10,np.uint8)
-				mask = cv.inRange(pixels1,(low_white), (max_white))	
-				cv.imshow('main1', mask)
-				cnts,hierarchy = cv.findContours(mask.copy(), cv.RETR_EXTERNAL,
-					cv.CHAIN_APPROX_SIMPLE)	
+        #starting photo analys
+        #res  = photo_analysis(img1)
+            res = 1
+            if res  == 1:
 
-				try:
-					for el in range(len(cnts)):
-						if cv.contourArea(cnts[el]) > 300 and cv.contourArea(cnts[el]) <= (640*320 / 10):
-							cv.drawContours(img1,cnts,el,(255,0,0),3,cv.LINE_AA,hierarchy,1)
-				except IndexError:
-					pass
+                # working with an image
+                # creating numpy masssive which takes information from blurreds
+                pixels1 = np.around(np.divide(diff, 0.1), decimals = 1)
+                cv.imshow("pixels",pixels1)
+                # modifying the image
 
-			draw()
-			cv.imshow('cntr',img1)
-			cv.imshow('countrs', image)
+                def draw():
 
-	raw.truncate(0)
-	counter = 1
-	time.sleep(0.1)
-	k = cv.waitKey(1)
-	if k%256 == 27:
-		break
+                    low_white = np.array(4,np.uint8) #for rasberry use 4
+                    max_white = np.array(10,np.uint8)
+                    mask = cv.inRange(pixels1,(low_white), (max_white))	
+                    cv.imshow('main1', mask)
+                    cnts,hierarchy = cv.findContours(mask.copy(), cv.RETR_EXTERNAL,
+                        cv.CHAIN_APPROX_SIMPLE)	
 
+                    try:
+                        for el in range(len(cnts)):
+                            if cv.contourArea(cnts[el]) > 300 and cv.contourArea(cnts[el]) <= (640*320 / 10):
+                                if cv.arcLength((cnts[el])**2 / cv.contourArea(cnts[el]))>= 60 and 300 >= cv.arcLength(cnts[el])**2 / cv.contourArea(cnts[el]):
+                                    
+                                    ((x, y), radius) = cv.minEnclosingCircle(cnts[el])
+                                    M = cv.moments(cnts[el])
+                                    center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+                                    l = [ (lambda i: random.randrange(0,256))(i) for i in range(3)]
+                                    cv.circle(img1, (int(x), int(y)), int(radius),
+                                                    (l[0],l[1],l[-1]), 2)
+                                    cv.circle(img1, center, 5, (0, 0, 255), -1)
+                                    cv.putText(img1, cv.contourArea(cnts[el]), (int(x), int(y)), cv.FONT_HERSHEY_SIMPLEX, (l[0],l[1],l[-1]), 2)
+
+                    except IndexError:
+                        pass
+
+                draw()
+                cv.imshow('cntr',img1)
+                cv.imshow('countrs', image)
+
+        raw.truncate(0)
+        counter = 1
+        time.sleep(0.1)
+        k = cv.waitKey(1)
+        if k%256 == 27:
+            break
