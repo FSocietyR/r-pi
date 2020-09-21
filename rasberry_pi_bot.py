b8163bec732addb6b2b0c78d66e84c037dcc0615
FSocietyR <48395407+FSocietyR@users.noreply.github.com>
2020-09-21 17:17:16 +0300

    Update rasberry_pi_bot.py

--- rasberry_pi_bot.py
+++ rasberry_pi_bot.py
@@ -31,5 +31,6 @@ def upload(file):
 				}
 	res = DRIVE.files().create(body=metadata, media_body=file_name).execute()
 	if res:
-		print('Uploaded "%s" (%s)') % (file_name, res['mimeType'])
+		print('Uploaded: {}'.format(file))
+                
 
